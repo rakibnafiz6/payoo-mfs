6289fbf606c0073bd38181a1781f1d1606eba835
nafiz rakib <rakibnafiz6@gmail.com>
2024-09-19 06:36:55 -0700

    added the home page money form

--- js/login.js
+++ js/login.js
@@ -4,7 +4,7 @@ document.getElementById('btn-login').addEventListener('click', function(event){
     const phoneNumber = document.getElementById('phone-number').value;
     const pinNumber = document.getElementById('pin-number').value;
     if(phoneNumber === '5' && pinNumber === '2008'){
-        
+        console.log('You are login ');
         window.location.href = '/home.html';
     }
     else{
